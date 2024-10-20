a21318f51c2c3827286932cc7e05feebbd096025
Fernando <mellolefloth@gmail.com>
2024-10-20 03:55:39 -0300

    Fix de la lógica de la vengana. Es un adapter del tipo que se hace con la clase. No utiliza una interfaz

--- src/ar/edu/unq/po2/tpTemplateMethod/ej7/ListaDePalabrasOrdenadasAdapter.java
+++ src/ar/edu/unq/po2/tpTemplateMethod/ej7/ListaDePalabrasOrdenadasAdapter.java
@@ -2,20 +2,14 @@ package ar.edu.unq.po2.tpTemplateMethod.ej7;
 
 import javax.swing.DefaultListModel;
 
-public class ListaDePalabrasOrdenadasAdapter extends DefaultListModel implements DefaultModelListInterface{
+public class ListaDePalabrasOrdenadasAdapter extends DefaultListModel { // ESTE ES EL TIPO DE ADAPTER QUE ES POR HERENCIA UNICA
 	private ListaDePalabrasOrdenadas lista;
 	
 	public ListaDePalabrasOrdenadasAdapter(ListaDePalabrasOrdenadas lista) {
 		this.lista = lista;
 	}
 
-	@Override
-	public void add(String palabra) {
-		lista.agregarPalabra(palabra);
-	}
-
-	@Override
-	public void addElement(String palabra) {
+	public void addElement(String palabra) { 
 		// TODO Auto-generated method stub
 		lista.agregarPalabra(palabra);
 	}
--- src/ar/edu/unq/po2/tpTemplateMethod/ej7/Ventana.java
+++ src/ar/edu/unq/po2/tpTemplateMethod/ej7/Ventana.java
@@ -7,29 +7,28 @@ import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.ListModel;
 
 public class Ventana extends JFrame{
-	public Ventana(DefaultModelListInterface listaDePalabrasOrdenadasAdapter){
+	public Ventana(DefaultListModel listModel){
 		 this.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
 		 this.setBounds(new Rectangle(180,180));
 		 this.setVisible(true);
 		 JLabel label=new JLabel("Lista de palabras");
 		 label.setBounds(new Rectangle(40,40));
 		 this.add(label);
-		 listaDePalabrasOrdenadasAdapter.addElement("casa");
-		 listaDePalabrasOrdenadasAdapter.addElement("arbol");
-		 listaDePalabrasOrdenadasAdapter.addElement("perro");
-		 listaDePalabrasOrdenadasAdapter.addElement("telefono");
-		 listaDePalabrasOrdenadasAdapter.addElement("brazo");
-		 JList lista=new JList((ListModel) listaDePalabrasOrdenadasAdapter); //ACA SE CASTEA: Esta raro esto
+		 listModel.addElement("casa");
+		 listModel.addElement("arbol");
+		 listModel.addElement("perro");
+		 listModel.addElement("telefono");
+		 listModel.addElement("brazo");
+		 JList lista=new JList(listModel);
 		 lista.setBounds(new Rectangle(110,160));
 		 this.add(lista);
 	}
 	public static void main(String[] args) {
 		
 		ListaDePalabrasOrdenadas listado = new ListaDePalabrasOrdenadas();
-		DefaultModelListInterface listaDePalabrasOrdenadasAdapter = new ListaDePalabrasOrdenadasAdapter(listado);
+		DefaultListModel<ListaDePalabrasOrdenadasAdapter> listaDePalabrasOrdenadasAdapter = new ListaDePalabrasOrdenadasAdapter(listado);
 		
 		new Ventana(listaDePalabrasOrdenadasAdapter);
 		
